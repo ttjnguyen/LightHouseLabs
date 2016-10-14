a616c56d3723416748445b0661dfbf5e1d03458e
ttjnguyen <ttjnguyen6@gmail.com>
2016-10-13 20:36:50 -0700

    If else statements

--- LightHouseLabs_FizzBuzz/main.m
+++ LightHouseLabs_FizzBuzz/main.m
@@ -10,8 +10,23 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+
+        for (int i = 1; i <= 100; i++) {
+            
+            if (i % 3 == 0 && i % 5 == 0) {
+                printf("FizzBuzz\n");
+            } else if (i % 3 == 0) {
+                printf("Fizz\n");
+            } else if (i % 5 == 0) {
+                printf("Buzz\n");
+            } else {
+                printf("%d\n", i);
+            }
+            
+        }
+        
+        
+        
     }
     return 0;
 }
